03326e76e4144d8cef1e9d1a15ce7792374c5447
Rubha Selvaraj <ruselvaraj@paypal.com>
2020-06-25 13:41:23 +0530

    Initial Commit

---
+++ SwiftUIWeatherApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUIWeatherApplication
+//
+//  Created by Rubha Selvaraj on 25/06/20.
+//  Copyright © 2020 Rubha Selvaraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
